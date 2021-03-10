815c60309ba9b17b002b21336c9d9d187f9a40be
leeyulls <feelriyou@naver.com>
2021-03-10 21:26:19 +0900

    nest 2일차 v2

--- my-nest/src/movies/movies.controller.ts
+++ my-nest/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,14 +8,21 @@ export class MoviesController {
         return "This will return all movies";
     }
 
+    @Get('search')
+    search(@Query('name') movieName:string){
+        return `영화 제목으로 검색중... : ${movieName}`;
+    }
+
     @Get("/:id")
     getOne(@Param('id') movieId:string){
         return `This will return one movie with the id: ${movieId}`;
     }
 
     @Post()
-    create(){
-        return 'This will create a movie';
+    create(@Body() movieData){
+        console.log(movieData);
+        //return 'This will create a movie';
+        return movieData;
     }
 
     @Delete("/:id")
@@ -27,9 +34,15 @@ export class MoviesController {
     //@Patch // 일부 업데이트
 
     @Patch('/:id')
-    patch(@Param('id') movieId:string){
-        return 'This will patch a movie : ' +movieId ;
+    patch(@Param('id') movieId:string, @Body() updateData){
+        //return 'This will patch a movie : ' +movieId ;
+        return {
+            updatedMovie : movieId,
+            ...updateData,
+        };       
     }
+
+  
 
 
 
